0991e05b12ab9307c91e293d7edcf4961253d6ef
YSH <sheepsh93@gmail.com>
2021-02-13 03:38:43 +0900

    동적 쿼리를 해결하는 방식 - Where 다중 파라미터 사용

--- src/test/java/sh/querydsl/QueryBasicTest.java
+++ src/test/java/sh/querydsl/QueryBasicTest.java
@@ -4,7 +4,9 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
@@ -718,4 +720,46 @@ public class QueryBasicTest {
                     .fetch();
     }
 
+
+    // Where 다중 파라미터 사용
+    @Test
+    public void dynamicQuery_WhereParam() throws Exception {
+
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember2(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
     }
+
+    private List<Member> searchMember2(String usernameCond, Integer ageCond) {
+
+        return queryFactory
+                .selectFrom(member)
+                .where(usernameEq(usernameCond),ageEq(ageCond)) //where 에 null값이 들어가면 그냥 무시한다.
+                //.where(allEq(usernameCond,ageCond)) //조립한거..
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String usernameCond) {
+
+        if (usernameCond == null) {
+            return null;
+        }
+        return member.username.eq(usernameCond);
+    }
+
+    private BooleanExpression ageEq(Integer ageCond){
+
+        if (ageCond == null) {
+            return null;
+        }
+        return member.age.eq(ageCond);
+    }
+
+    //이런식으로 위에 두 개를 합쳐서 조립도 가능하다.
+    private BooleanExpression allEq(String usernameCond,Integer ageCond) {
+
+        return usernameEq(usernameCond).and(ageEq(ageCond));
+    }
+}
