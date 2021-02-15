83e8c449c5459e17548079854573db5403a8ca56
YSH <sheepsh93@gmail.com>
2021-02-15 22:24:10 +0900

    조회용 API 컨트롤러 개발 테스트코드에 영향이 가지 않게 application.yml을 local과 test로 각각 분리함. 실행시, 바로 초기화되어 값이 들어가게 InitMember를 생성하고, MemberController를 생성하여 들어있는 값을 조회할 수 있게 함.

---
+++ src/main/java/sh/querydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package sh.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sh.querydsl.dto.MemberSearchCondition;
+import sh.querydsl.dto.MemberTeamDto;
+import sh.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
