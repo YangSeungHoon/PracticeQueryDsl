ff60c1cfd9fedc1f7f0c36d04cb75f1b1a809a4b
YSH <sheepsh93@gmail.com>
2021-02-16 08:47:01 +0900

    스프링 데이터 페이징 활용 - searchPageSimple 실험용 맵핑 MemberController에 추가.

--- src/main/java/sh/querydsl/controller/MemberController.java
+++ src/main/java/sh/querydsl/controller/MemberController.java
@@ -24,9 +24,20 @@ public class MemberController {
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
     }
+
+
 //http://localhost:8080/v2/members?ageGoe=99&page=0&size=50
+    //전체 데이터는 100개 넣어놓은 상태인데 페이징 사이즈를 50개로 했다.그리고 조건에 99살 이사응로 조건을 걸었기 때문에 조건에 부합하는 값은 많아야
+    //5개를 넘지 못한다. 그렇기 때문에 위의 경우, count쿼리를 날리지 않는다.
     @GetMapping("/v2/members")
     public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
         return memberRepository.searchPageUpgrade(condition,pageable);
     }
+
+    //http://localhost:8080/v3/members?page=0&size=5
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageUpgrade(condition,pageable);
+    }
+
 }
