0b89807ffb013b4db1c8d1dabfd00ad2cca00934
YSH <sheepsh93@gmail.com>
2021-02-17 09:12:35 +0900

    리포지토리 지원 - QuerydslRepositorySupport

--- src/main/java/sh/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/sh/querydsl/repository/MemberRepositoryImpl.java
@@ -2,11 +2,13 @@ package sh.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import sh.querydsl.dto.MemberSearchCondition;
 import sh.querydsl.dto.MemberTeamDto;
@@ -20,58 +22,67 @@ import static org.springframework.util.StringUtils.hasText;
 import static sh.querydsl.entity.QMember.member;
 import static sh.querydsl.entity.QTeam.team;
 
-//MemberRepository에다가 할 것 이기때문에, 이름을 뒤에 Impl을 붙여줘야하는 규칙이있다.
-public class MemberRepositoryImpl implements MemberRepositoryCustom{
+//QuerydslRepositorysupport의 장점으로는 getQuerydsl().applyPagination()으로 스프링 데이터가 제공하는 페이징을 Querydsl로
+//편리하게 변환 가능하다. 더하여 EntityManager를 제공한다.
+//그러나 from절로 시작해야하는 단점과 스프링 데이터 Sort기능이 정상 동작하지 않는다는 불편함이있다.
 
-    private final JPAQueryFactory queryFactory;
+//QuerydslRepositorySupport를 extends해주면 이것이 제공하는 기능을 사용할 수 있다.
+public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom{
 
-    public MemberRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
+//    private final JPAQueryFactory queryFactory;
+//
+//    public MemberRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    } 이 코드들이 아래의 super로 다 대체가 되는 것.
+
+    //QuerydslRepositorySupport는 알아서 EntityManager까지 사용할 수 있게 해준다.
+    public MemberRepositoryImpl() {
+        super(Member.class);
     }
 
     public List<MemberTeamDto> search(MemberSearchCondition condition) {
-        return queryFactory
-                .select(new QMemberTeamDto(
-                        member.id.as("memberId"),
-                        member.username,
-                        member.age,
-                        team.id.as("teamId"),
-                        team.name.as("teamName")))
-                .from(member)
-                .leftJoin(member.team,team)
-                .where(usernameEq(condition.getUsername()),
-                        teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe())
-                )
-                .fetch();
-    }
 
-    @Override
-    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        //이전에 3버전때는 from부터 쿼리문을 만들었던 규칙이 있어서 그 규칙대로 쿼리를 짠다.
 
-        QueryResults<MemberTeamDto> results = queryFactory
-                .select(new QMemberTeamDto(
-                        member.id.as("memberId"),
-                        member.username,
-                        member.age,
-                        team.id.as("teamId"),
-                        team.name.as("teamName")))
-                .from(member)
+        return from(member)
                 .leftJoin(member.team, team)
                 .where(usernameEq(condition.getUsername()),
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
                 )
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetchResults(); //Querydsl이 content용 쿼리, count용 쿼리 두 개를 날린다.
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .fetch();
+    }
 
-        List<MemberTeamDto> content = results.getResults();
-        long total = results.getTotal();
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
 
-        return new PageImpl<>(content,pageable,total);
+        //페이징 처리도 기존에 따로 offset과 limit을 설정하던 것과 달리, 아래에서 getQuerydsl().applyPagination을 이용하여 처리가
+        //가능하다.
+        JPQLQuery<MemberTeamDto> jpaQuery = from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ).select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")));
+
+        JPQLQuery<MemberTeamDto> query = getQuerydsl().applyPagination(pageable, jpaQuery);
+
+        query.fetch();
+
+
     }
 
     @Override
@@ -144,10 +155,6 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         ageLoe(condition.getAgeLoe())
                 );
 
-        //예를들어 전체 페이지는 100개인데 그 검색 결과 조건에 부합하는 content는 1개 밖에 없다면 그 페이지를 Counting할 필요가 없다.
-        //왜냐하면 content의 갯수가 페이지 수를 넘지 못해서 어차피 1일거니까.. 그래서 이러한 경우 아래 함수식은 실행되지 않아서, 따로 쿼리를
-        //날리지 않기때문에 최적화가 이루어진다.
-        //return PageableExecutionUtils.getPage(content,pageable,() -> countQuery.fetchCount());
         return PageableExecutionUtils.getPage(content,pageable, countQuery::fetchCount);
     }
 
@@ -167,3 +174,151 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
         return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 }
+
+////MemberRepository에다가 할 것 이기때문에, 이름을 뒤에 Impl을 붙여줘야하는 규칙이있다.
+//public class MemberRepositoryImpl implements MemberRepositoryCustom{
+//
+//    private final JPAQueryFactory queryFactory;
+//
+//    public MemberRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    }
+//
+//    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+//        return queryFactory
+//                .select(new QMemberTeamDto(
+//                        member.id.as("memberId"),
+//                        member.username,
+//                        member.age,
+//                        team.id.as("teamId"),
+//                        team.name.as("teamName")))
+//                .from(member)
+//                .leftJoin(member.team,team)
+//                .where(usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+//                )
+//                .fetch();
+//    }
+//
+//    @Override
+//    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+//
+//        QueryResults<MemberTeamDto> results = queryFactory
+//                .select(new QMemberTeamDto(
+//                        member.id.as("memberId"),
+//                        member.username,
+//                        member.age,
+//                        team.id.as("teamId"),
+//                        team.name.as("teamName")))
+//                .from(member)
+//                .leftJoin(member.team, team)
+//                .where(usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+//                )
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize())
+//                .fetchResults(); //Querydsl이 content용 쿼리, count용 쿼리 두 개를 날린다.
+//
+//        List<MemberTeamDto> content = results.getResults();
+//        long total = results.getTotal();
+//
+//        return new PageImpl<>(content,pageable,total);
+//    }
+//
+//    @Override
+//    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+//
+//        List<MemberTeamDto> content = queryFactory
+//                .select(new QMemberTeamDto(
+//                        member.id.as("memberId"),
+//                        member.username,
+//                        member.age,
+//                        team.id.as("teamId"),
+//                        team.name.as("teamName")))
+//                .from(member)
+//                .leftJoin(member.team, team)
+//                .where(usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+//                )
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize())
+//                .fetch();
+//
+//        long total = queryFactory
+//                .select(member)
+//                .from(member)
+//                .leftJoin(member.team, team)
+//                .where(
+//                        usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+//                )
+//                .fetchCount();
+//
+//        return new PageImpl<>(content,pageable,total);
+//
+//
+//    }
+//
+//    @Override
+//    public Page<MemberTeamDto> searchPageUpgrade(MemberSearchCondition condition, Pageable pageable) {
+//
+//        List<MemberTeamDto> content = queryFactory
+//                .select(new QMemberTeamDto(
+//                        member.id.as("memberId"),
+//                        member.username,
+//                        member.age,
+//                        team.id.as("teamId"),
+//                        team.name.as("teamName")))
+//                .from(member)
+//                .leftJoin(member.team, team)
+//                .where(usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+//                )
+//                .offset(pageable.getOffset())
+//                .limit(pageable.getPageSize())
+//                .fetch();
+//
+//        JPAQuery<Member> countQuery = queryFactory
+//                .select(member)
+//                .from(member)
+//                .leftJoin(member.team, team)
+//                .where(
+//                        usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe())
+//                );
+//
+//        //예를들어 전체 페이지는 100개인데 그 검색 결과 조건에 부합하는 content는 1개 밖에 없다면 그 페이지를 Counting할 필요가 없다.
+//        //왜냐하면 content의 갯수가 페이지 수를 넘지 못해서 어차피 1일거니까.. 그래서 이러한 경우 아래 함수식은 실행되지 않아서, 따로 쿼리를
+//        //날리지 않기때문에 최적화가 이루어진다.
+//        //return PageableExecutionUtils.getPage(content,pageable,() -> countQuery.fetchCount());
+//        return PageableExecutionUtils.getPage(content,pageable, countQuery::fetchCount);
+//    }
+//
+//    private BooleanExpression usernameEq(String username) {
+//        return hasText(username) ? member.username.eq(username) : null;
+//    }
+//
+//    private BooleanExpression teamNameEq(String teamName) {
+//        return hasText(teamName) ? team.name.eq(teamName) : null;
+//    }
+//
+//    private BooleanExpression ageGoe(Integer ageGoe) {
+//        return ageGoe != null ? member.age.goe(ageGoe) : null;
+//    }
+//
+//    private BooleanExpression ageLoe(Integer ageLoe) {
+//        return ageLoe != null ? member.age.loe(ageLoe) : null;
+//    }
+//}
